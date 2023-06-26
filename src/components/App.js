aba139e3c351aa697d6f321bda51ea378f71f863
Dnnguyen <dnnguyen100@gmail.com>
2023-06-26 15:37:40 -0400

    add contact list array to app.jsand passed it as a prop to contact list

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,28 @@ import Addcontact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Dipesh",
+      email: "maivia@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Nipove",
+      email: "ghaedtiy@gmail.com",
+    },
+    {
+      id: "3",
+      name: "Basht",
+      email: "laneshe@gmail.com",
+    },
+  ];
   return (
-    <div>
+    <div className="ui container">
       <Header />
       <Addcontact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
